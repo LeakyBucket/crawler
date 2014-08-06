3df19626684fe85037dffc3f36a0ae08c3ddcf38
Glen Holcomb <gholcomb@gholcomb01.local>
2014-08-06 13:23:10 -0600

    Adds `Map` for storage of results.
    * `Crawler::Map#add` adds a page to the map.
    * `Crawler::Map#static_assets` stores all assets found during the crawl.
    * `Crawler::Map#site` is the actual `site map`.  This is a simple hash keyed with `URL`s and with each bucket holding a collection of links and asset references.

---
+++ lib/crawler/map.rb
@@ -0,0 +1,26 @@
+module Crawler
+  class Map
+    attr_reader :site, :static_assets
+
+    def initialize
+      @site = {}
+      @static_assets = Set.new
+    end
+
+    def add(page)
+      site[page.location] = {
+        links: page.links,
+        assets: assets_index_for(page)
+      }
+    end
+
+    private
+
+    def assets_index_for(page)
+      page.assets.map do |asset|
+        static_assets.add asset
+        static_assets.find_index asset
+      end
+    end
+  end
+end
---
+++ spec/crawler/map_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe Crawler::Map do
+  subject(:site_map) { described_class.new }
+  let(:url) { 'https://www.google.com' }
+
+  let(:links) do
+    [
+      'http://www.google.com/fake/path',
+      'http://www.google.com/another/fake/path'
+    ]
+  end
+
+  let(:assets) do
+    [
+      'styles.css',
+      'behaviors.js'
+    ]
+  end
+
+  let(:page) do
+    page = double
+
+    allow(page).to receive(:location).and_return(url)
+    allow(page).to receive(:links).and_return(links)
+    allow(page).to receive(:assets).and_return(assets)
+
+    page
+  end
+
+  describe "#add" do
+    before do
+      site_map.add page
+    end
+
+    it "adds the pages links" do
+      page_record = site_map.site[url]
+
+      expect(page_record[:links]).to eq links
+    end
+
+    it "adds the assets to the static_assets collection" do
+      expect(site_map.static_assets).to include 'styles.css', 'behaviors.js'
+    end
+
+    it "adds the assets indexes to the page entry" do
+      page_record = site_map.site[url]
+
+      expect(page_record[:assets]).to include 0, 1
+    end
+  end
+end
