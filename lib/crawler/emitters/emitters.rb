87da602f3457f8f72a2bb2d4a306a56129fdf354
Glen Holcomb <gholcomb@gholcomb01.local>
2014-08-09 16:18:43 -0600

    Returns a more semantic error when an unsupported Emitter format is requested.

--- lib/crawler/emitters/emitters.rb
+++ lib/crawler/emitters/emitters.rb
@@ -1,7 +1,11 @@
 module Crawler
   module Emitters
     def self.new(format)
-      self.const_get(constantize format).new
+      emitter = self.const_get(constantize format)
+
+      emitter.new
+    rescue NameError => e
+      raise UnknownFormat, "Failed to find an emitter for the #{format} format"
     end
 
     private
@@ -9,5 +13,7 @@ module Crawler
     def self.constantize(format)
       format.to_s.split('_').map{ |part| part.capitalize }.join('')
     end
+
+    class UnknownFormat < StandardError; end
   end
 end
--- spec/crawler/emitters/emitters_spec.rb
+++ spec/crawler/emitters/emitters_spec.rb
@@ -7,5 +7,9 @@ describe Crawler::Emitters do
     it "returns an instance of the specified emitter" do
       expect(emitter.new :dot).to be_kind_of Crawler::Emitters::Dot
     end
+
+    it "raises UnknownFormat if given a bad format symbol" do
+      expect { emitter.new :bogus }.to raise_exception(Crawler::Emitters::UnknownFormat)
+    end
   end
 end
